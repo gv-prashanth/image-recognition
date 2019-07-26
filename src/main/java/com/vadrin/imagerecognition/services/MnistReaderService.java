7b44190a8346ff047cbe41ae34c4ce4103d41601
Prashanth Venkat Gumma <pragumma0@publicisgroupe.net>
2019-07-26 13:53:56 +0530

    issue with file reference in server environments

--- src/main/java/com/vadrin/imagerecognition/services/MnistReaderService.java
+++ src/main/java/com/vadrin/imagerecognition/services/MnistReaderService.java
@@ -2,14 +2,13 @@ package com.vadrin.imagerecognition.services;
 
 import static java.lang.String.format;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
@@ -26,11 +25,11 @@ public class MnistReaderService {
 	private static final String TEST_LABLES_FILE_NAME = "static/t10k-labels.idx1-ubyte";
 	
 	//TODO: Since MNIST is 28 by 28 images. This method is hardcoded to 28*28 double
-	private DataSet createSet(String imagesLoc, String lablesLoc) {
+	private DataSet createSet(InputStream trainingInputStream, InputStream lableInputStream) {
 		DataSet set = new DataSet();
 		try {
-			int[] labels = getLabels(lablesLoc);
-			List<int[][]> images = getImages(imagesLoc);
+			int[] labels = getLabels(lableInputStream);
+			List<int[][]> images = getImages(trainingInputStream);
 			for (int i = 0; i < labels.length; i++) {
 				double[] input = new double[28 * 28];
 				double[] output = new double[10];
@@ -49,18 +48,18 @@ public class MnistReaderService {
 	}
 	
 	public DataSet getTrainingSet() throws IOException {
-		return createSet(new ClassPathResource(TRAINING_IMAGES_FILE_NAME).getFile().getPath(),
-				new ClassPathResource(TRAINING_LABLES_FILE_NAME).getFile().getPath());
+		return createSet(new ClassPathResource(TRAINING_IMAGES_FILE_NAME).getInputStream(),
+				new ClassPathResource(TRAINING_LABLES_FILE_NAME).getInputStream());
 	}
 	
 	public DataSet getTestSet() throws IOException {
-		return createSet(new ClassPathResource(TEST_IMAGES_FILE_NAME).getFile().getPath(),
-				new ClassPathResource(TEST_LABLES_FILE_NAME).getFile().getPath());
+		return createSet(new ClassPathResource(TEST_IMAGES_FILE_NAME).getInputStream(),
+				new ClassPathResource(TEST_LABLES_FILE_NAME).getInputStream());
 	}
 	
-	protected int[] getLabels(String infile) {
+	protected int[] getLabels(InputStream inputStream) {
 
-		ByteBuffer bb = loadFileToByteBuffer(infile);
+		ByteBuffer bb = loadStreamToByteBuffer(inputStream);
 
 		assertMagicNumber(LABEL_FILE_MAGIC_NUMBER, bb.getInt());
 
@@ -73,8 +72,8 @@ public class MnistReaderService {
 		return labels;
 	}
 
-	protected List<int[][]> getImages(String infile) {
-		ByteBuffer bb = loadFileToByteBuffer(infile);
+	protected List<int[][]> getImages(InputStream inputStream) {
+		ByteBuffer bb = loadStreamToByteBuffer(inputStream);
 
 		assertMagicNumber(IMAGE_FILE_MAGIC_NUMBER, bb.getInt());
 
@@ -122,24 +121,14 @@ public class MnistReaderService {
 	 * them. ;-)
 	 ******/
 
-	private ByteBuffer loadFileToByteBuffer(String infile) {
-		return ByteBuffer.wrap(loadFile(infile));
+	private ByteBuffer loadStreamToByteBuffer(InputStream inputStream) {
+		return ByteBuffer.wrap(loadFile(inputStream));
 	}
 
-	private byte[] loadFile(String infile) {
+	private byte[] loadFile(InputStream inputStream) {
 		try {
-			RandomAccessFile f = new RandomAccessFile(infile, "r");
-			FileChannel chan = f.getChannel();
-			long fileSize = chan.size();
-			ByteBuffer bb = ByteBuffer.allocate((int) fileSize);
-			chan.read(bb);
-			bb.flip();
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			for (int i = 0; i < fileSize; i++)
-				baos.write(bb.get());
-			chan.close();
-			f.close();
-			return baos.toByteArray();
+			byte[] bytes = IOUtils.toByteArray(inputStream);
+			return bytes;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
